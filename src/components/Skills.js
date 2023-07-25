86889a741af6acb626d5845f5800ec73657537d8
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-07-25 16:52:40 +0200

    fixed margin problem

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,7 @@ function Skills() {
   return (
     <div>
       <div className="text-4xl px-1 ">
-        <div className="p-2 m-10 text-2xl md:text-3xl lg:text-3xl">
+        <div className="text-2xl p-2 m-8 md:text-3xl lg:text-3xl">
           <div className="flex items-center">
             <span className="mx-2" id="skills">
               SKILLS
